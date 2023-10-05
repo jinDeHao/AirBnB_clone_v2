300c89c153b60fed1cb74a40f15a70ce1bda586f
omarIdhmaid <o.idhmaid@gmail.com>
2023-10-05 23:35:36 +0100

    fab gen.tgz

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,7 +10,8 @@ def do_pack():
     """function do pack that generate .tgz"""
     fab.local("ls versions > /dev/null 2>&1 || mkdir versions")
     date = datetime.now().strftime("%Y%m%d%H%M%S")
-    tgz = fab.local("tar -cvzf versions/web_static_{}.tgz web_static".format(date))
+    tgz = fab.local("tar -cvzf versions/web_static_{}.tgz web_static\
+".format(date))
     if tgz.succeeded:
         return "versions/web_static_{}.tgz".format(date)
     return None
