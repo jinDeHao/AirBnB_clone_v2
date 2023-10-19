99113b7d82154c55d0699c4ac1b069358959f5a7
omarIdhmaid <o.idhmaid@gmail.com>
2023-10-20 00:27:13 +0100

    fix pip8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""run """
+"""run flask server"""
 from flask import Flask
 app = Flask(__name__)
+
+
 @app.route("/", strict_slashes=False)
 def hello_world():
+    """hello returned"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
